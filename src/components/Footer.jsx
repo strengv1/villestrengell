02a8134fb3daa92085e32695f94e2d9af991b77b
Ville Strengell <villes-97@hotmail.com>
2023-10-28 16:23:35 +0300

    added raccoon svg and moving box

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,19 @@
 const Footer = () => (
   
   <div className="footer container">
-    <div className="content">
+    <div className="content container">
       
       <div className="row">
         <p className="common-paragraph">
           This is the footer
         </p>
       </div>
+   
+      <div className="moving-square-container">
+        <div className="moving-square"></div>
+      </div>
+      
+      
     </div>
   </div>
 )
