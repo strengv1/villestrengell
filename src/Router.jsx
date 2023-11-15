18225f2f72f13f767d432638da56cf2061f465fa
Ville Strengell <villes-97@hotmail.com>
2023-11-15 11:13:10 +0200

    fix Not found issue?

--- src/Router.jsx
+++ src/Router.jsx
@@ -24,6 +24,7 @@ const router = createBrowserRouter(
           element={<Game />}
           loader={scoreLoader} />
       </Route>
+      <Route path="*" element={<App />} />
     </Route>
   )
 )
